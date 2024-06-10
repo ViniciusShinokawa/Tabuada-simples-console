079c6b898f04a2d6d3ab665c9ab45c09f07e1b31
Vinicius Shinokawa <viniciusakutagawa24@gmail.com>
2024-06-10 15:52:19 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+
+        public static void Tabuada(int numero)
+        {
+            Console.WriteLine("=========================================");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(numero + "X" + i + " = " + (numero * i));
+            }
+            Console.WriteLine("=========================================");
+          
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Tabuada(1);
+            Tabuada(2);
+
+        }
+    }
+}
